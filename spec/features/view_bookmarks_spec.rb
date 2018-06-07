17715035780013ccacb2a14e87d0c69620d54d31
Laura Petre <laura.d.petre@gmail.com>
2018-06-07 12:19:02 +0100

    11 Wrapping databases fixed

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -3,14 +3,14 @@ require 'pg'
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
 
-    Bookmark.create(url: "http://makersacademy.com")
-    Bookmark.create(url: "http://destroyallsoftware.com")
-    Bookmark.create(url: "http://google.com")
+    Bookmark.create(url: "http://makersacademy.com", title: 'MakersAcademy')
+    Bookmark.create(url: "http://destroyallsoftware.com",title: 'Destroy')
+    Bookmark.create(url: "http://google.com",title: 'Google')
 
     visit('/bookmarks')
-    
-    expect(page).to have_content "http://makersacademy.com"
-    expect(page).to have_content "http://destroyallsoftware.com"
-    expect(page).to have_content "http://google.com"
+
+    expect(page).to have_content "MakersAcademy"
+    expect(page).to have_content "Destroy"
+    expect(page).to have_content "Google"
   end
 end
